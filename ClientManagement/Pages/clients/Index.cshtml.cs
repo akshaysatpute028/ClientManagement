e4780749592aee14b94374cacc6ae2b18341eacc
akshaysatpute028 <satputeakshay028@gmail.com>
2022-08-19 00:35:58 +0530

    commit 1

--- ClientManagement/Pages/clients/Index.cshtml.cs
+++ ClientManagement/Pages/clients/Index.cshtml.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
-namespace ClientManagement.Pages.clients
+namespace ClientManagement.Pages.Clients
 {
     public class IndexModel : PageModel
     {
@@ -11,20 +12,46 @@ namespace ClientManagement.Pages.clients
         {
             try
             {
-                String connectionString = "";
-            }
-            catch
-            {
+                string connectionString = "Data Source=LAPTOP-19LGOJJ2\\SQLEXPRESS;Initial Catalog=clients;Persist Security Info=True;User ID=sa;Password=cdac123;Pooling=False";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM client_info";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while(reader.Read())
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
 
+                                clientsList.Add(clientInfo);
+
+                                Console.WriteLine("Collection : " + clientsList[0]);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception : "+ex.ToString());
             }
         }
     }
     public class ClientInfo
     {
-        public string id;
-        public string name;
-        public string email;
-        public string phone;
-        public string address;
+        public string id="";
+        public string name="";
+        public string email="";
+        public string phone="";
+        public string address ="";
+        public string created_at="";
     }
 }
